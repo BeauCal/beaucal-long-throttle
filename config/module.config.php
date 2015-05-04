088c9a8928d80143efc63e2a97e6d2942b0aab11
Derek Illchuk <dillchuk@gmail.com>
2015-05-04 14:54:56 -0500

    BeaucalLongThrottle alias to 0.1.0.

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,9 @@
 
 return [
     'service_manager' => [
+        'aliases' => [
+            'BeaucalLongThrottle' => 'BeaucalLongThrottle\Service\Throttle',
+        ],
         'factories' => [
             'beaucallongthrottle_config' => 'BeaucalLongThrottle\Factory\ConfigFactory',
             'BeaucalLongThrottle\Service\Throttle' => 'BeaucalLongThrottle\Factory\ThrottleFactory',
