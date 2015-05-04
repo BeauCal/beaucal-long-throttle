303954d9b613463d831fc2f93c2898117493194f
Derek Illchuk <dillchuk@gmail.com>
2015-05-03 20:39:11 -0500

    Moved Module.php into /src/BeaucalLongThrottle/

--- Module.php
+++ Module.php
@@ -1,23 +1,3 @@
 <?php
 
-namespace BeaucalLongThrottle;
-
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-
-class Module implements AutoloaderProviderInterface {
-
-    public function getConfig() {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig() {
-        return [
-            'Zend\Loader\StandardAutoloader' => [
-                'namespaces' => [
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ],
-            ],
-        ];
-    }
-
-}
+require_once __DIR__ . '/src/BeaucalLongThrottle/Module.php';
