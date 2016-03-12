c0dd19f511e8a2c326e44b31ab3c9788c816c2ef
Derek Illchuk <dillchuk@gmail.com>
2016-03-11 19:22:54 -0600

    Travis CI setup.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
 
-if (file_exists('../vendor/autoload.php')) {
-    include '../vendor/autoload.php';
-} elseif (file_exists('../../../autoload.php')) {
-    include '../../../autoload.php';
-} else {
-    throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
+foreach (['.', '..', '../../..'] as $dir) {
+    $autoload = "{$dir}/vendor/autoload.php";
+    if (file_exists($autoload)) {
+        include $autoload;
+        return;
+    }
 }
+throw new RuntimeException('vendor/autoload.php could not be found. Did you run `php composer.phar install`?');
---
+++ tests/dbadapter.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'db' => [
+        'username' => 'beaucallongthrottle',
+        'password' => 'beaucallongthrottle',
+        'dsn' => 'mysql:dbname=beaucallongthrottle;host=localhost;charset=utf8'
+    ]
+];
