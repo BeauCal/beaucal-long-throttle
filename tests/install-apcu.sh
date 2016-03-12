9cb100e52edf19cb53be5043f48a9948830637ef
Derek Illchuk <dillchuk@gmail.com>
2016-03-12 10:20:46 -0600

    Travis CI setup.

---
+++ tests/install-apcu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$TRAVIS_PHP_VERSION" == "5.4"  ]; then
+    exit 0
+fi
+
+# this is helpful to compile extension
+sudo apt-get install autoconf
+
+pecl install apcu
+
+# compile manually, because `pecl install apcu-beta` keep asking questions
+# APCU=4.0.2
+# get http://pecl.php.net/get/apcu-$APCU.tgz
+# tar zxvf apcu-$APCU.tgz
+# cd "apcu-${APCU}"
+# phpize && ./configure && make install && echo "Installed ext/apcu-${APCU}"
